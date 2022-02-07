1ef22ca885576d297622bb62d3435a0c1d0ac7d3
juggledad <juggledad@users.noreply.github.com>
2022-02-07 06:31:39 -0500

    change to pypi-
    change to using pypi- instead of pypi_

--- testpypi.sh
+++ testpypi.sh
@@ -3,5 +3,6 @@
 cd $HOME
 mkdir .pypiinfo
 cd .pypiinfo
-curl -sL -o pypi_info.service.temp https://raw.githubusercontent.com/juggledad/pypi_info/main/pypi_info.service
-curl -sL -o pypi_info.service.temp https://raw.githubusercontent.com/juggledad/pypi_info/main/pypi_info.py
+curl -sL -o pypi_info.service.temp https://raw.githubusercontent.com/juggledad/pypi-info/main/pypi-info.service
+curl -sL -o pypi_info.service.temp https://raw.githubusercontent.com/juggledad/pypi-info/main/pypi-info.py
+curl -sL -o pypi_info.service.temp https://raw.githubusercontent.com/juggledad/pypi-info/main/pypi-config.py
