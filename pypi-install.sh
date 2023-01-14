538c9e4b82de1aa5a0eb7465cf477ef62508ebca
juggledad <juggledad@users.noreply.github.com>
2023-01-14 05:40:22 -0500

    Moving  things around
    pypi-info.py and pypiconfig.py will be put in /usr/local/bin
    pypi-info.service goes into /etc/systemd/user

--- pypi-install.sh
+++ pypi-install.sh
@@ -3,27 +3,28 @@
 umask 0022      # give 'group' and 'others' write permission 
 
 cd $HOME
-mkdir .pypiinfo
-cd .pypiinfo
+#mkdir .pypiinfo
+#cd .pypiinfo
 
 echo "====> get service file and pypi-info.py"
-curl -sL -o pypi-info.service https://raw.githubusercontent.com/juggledad/pypi-info/main/pypi-info.service
-curl -sL -o pypi-info.py https://raw.githubusercontent.com/juggledad/pypi-info/main/pypi-info.py 
-curl -sL -o pypiconfig.py https://raw.githubusercontent.com/juggledad/pypi-info/main/pypiconfig.py 
+curl -sL -o /etc/systemd/user/pypi-info.service https://raw.githubusercontent.com/juggledad/pypi-info/main/pypi-info.service
+curl -sL -o /usr/local/bin/pypi-info.py https://raw.githubusercontent.com/juggledad/pypi-info/main/pypi-info.py 
+curl -sL -o /usr/local/bin/pypiconfig.py https://raw.githubusercontent.com/juggledad/pypi-info/main/pypiconfig.py 
+
 
 echo "====> remove old pypi_info.service and pypi-info.service file if they exists"
 sudo rm /lib/systemd/system/pypi_info.service
 sudo rm /lib/systemd/system/pypi-info.service
 
-echo "====> move pypi-info.service.temp to /lib/systemd/system/ and rename"
-sudo mv pypi-info.service /lib/systemd/system/pypi-info.service
+#echo "====> move pypi-info.service.temp to /lib/systemd/system/ and rename"
+#sudo mv pypi-info.service /lib/systemd/system/pypi-info.service
 
-echo "====> install python3-pip and paho-mqtt"
-sudo apt update
+#echo "====> install python3-pip and paho-mqtt"
+#sudo apt update
 #sudo apt upgrade -y
-sudo apt install python3 -y
-sudo apt install python3-pip -y
-sudo pip3 install paho-mqtt
+#sudo apt install python3 -y
+#sudo apt install python3-pip -y
+#sudo pip3 install paho-mqtt
 
 echo "====> enable the pypi-info.service"
 sudo systemctl enable pypi-info.service
